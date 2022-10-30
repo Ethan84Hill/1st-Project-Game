f3749da688a3d88e8be1559184388e02e4dd1584
Ethan <ethan84hill@gmail.com>
2022-10-30 13:53:06 -0400

    added image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 console.log('hello')
+
+window.onload = () => {
+    document.getElementById('start-now').onclick = () => {
+      startGame();
+    };
+    
+    function startGame() {
+        const canvas = document.getElementById('canvas');
+        const ctx = canvas.getContext('2d');
+
+
+
+    }
+
+
+
+
+
+}
