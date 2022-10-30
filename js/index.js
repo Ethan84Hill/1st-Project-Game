9c9b1aa434725a71294c7f7eaba308c67f346430
Ethan <ethan84hill@gmail.com>
2022-10-30 16:50:53 -0400

    set up done

--- js/index.js
+++ js/index.js
@@ -11,6 +11,38 @@ window.onload = () => {
 
 
 
+        const imgTrack = new Image();
+        imgTrack.src = 'images/road.png';
+        ctx.drawImage(imgTrack, 0, 0, 1015, 710)
+
+        let xCord = 200;
+        let yCord = 620;
+       const imgCar = new Image();
+       imgCar.src = 'images/F1_car.png'
+      ctx.drawImage(imgCar, xCord, yCord, 30, 60)
+      window.addEventListener('keydown', function(event) {
+        ctx.clearRect(0, 0, 1015, 710)
+        ctx.drawImage(imgTrack, 0, 0, 1015, 710)
+        ctx.drawImage(imgCar, xCord, yCord, 50, 90)
+        switch(event.code) {
+          case 'ArrowRight':
+          xCord += 10;
+           break;
+          case 'ArrowLeft':
+            xCord -= 10;
+            break;
+        case 'ArrowUp':
+            yCord -= 10;
+            break;
+        case 'ArrowDown':
+            yCord += 10;
+            break;
+        }
+      })
+
+
+
+
     }
 
 
